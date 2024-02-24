56445205cf5af06a6062043cf4de1036b856a143
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:22:26 +0500

    Add files via upload

---
+++ q8.js
@@ -0,0 +1,14 @@
+"use strict";
+/* Ques # 08:
+
+You should create four lines that look like this:
+
+___________________________________________________
+
+console.log(5+3)
+Your output should simply be four lines with the num 8 appering once on each line.
+*/
+console.log(5 + 3);
+console.log(18 - 10);
+console.log(2 * 4);
+console.log(16 / 2);
